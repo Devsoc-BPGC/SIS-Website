1bb1d6211ac40a91670e6dbc9559b9f1af8bfa64
Abheshek Murugan <abheshek.murugan@gmail.com>
2023-11-02 23:05:45 +0530

    Added line 2 and 3

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,19 @@
 import Navbar from "./HomePage/navbar";
 
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
 import Line1 from "./HomePage/line1";
+import Line2 from "./HomePage/line2";
+import Line3 from "./HomePage/line3";
 
-export default function Home () {
-    return (
-        <>
-            <ChakraProvider>
-                <Navbar />
-                <Line1 />
-            </ChakraProvider>
-        </>
-    );
+export default function Home() {
+  return (
+    <>
+      <ChakraProvider>
+        <Navbar />
+        <Line1 />
+        <Line2 />
+        <Line3 />
+      </ChakraProvider>
+    </>
+  );
 }
