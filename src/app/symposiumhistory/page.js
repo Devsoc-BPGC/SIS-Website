fb734262c58427065209585f4b14878b5ca9381e
ruchikxxii <ruchik710@gmail.com>
2023-11-28 14:56:27 +0530

    overview and histroy

--- src/app/symposiumhistory/page.js
+++ src/app/symposiumhistory/page.js
@@ -1,9 +1,140 @@
-"use client"
+"use client";
 import Puc from "../HomePage/Puc";
-import { Box } from "@chakra-ui/react";
+import {
+  Flex,
+  Box,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+  Heading,
+} from "@chakra-ui/react";
 import React from "react";
 export default function symposiumhistory() {
-return(
-    <Puc></Puc>
-)
+  const data = [
+    {
+      y: 1976,
+      l: "Albany, NY, USA",
+    },
+    {
+      y: 1978,
+      l: "Knoxville, TN, USA",
+    },
+    {
+      y: 1980,
+      l: "Potsdam,NY, USA",
+    },
+    {
+      y: 1982,
+      l: "Lund, Sweden",
+    },
+    {
+      y: 1984,
+      l: "Bordeaux, France",
+    },
+    {
+      y: 1986,
+      l: "New Delhi, India",
+    },
+    {
+      y: 1988,
+      l: "Ottawa, Canada",
+    },
+    {
+      y: 1992,
+      l: "Varna, Bulgaria",
+    },
+    {
+      y: 1994,
+      l: "Caracas, Venezuela",
+    },
+    {
+      y: 1996,
+      l: "Jerusalem, Israel",
+    },
+    {
+      y: 1998,
+      l: "Stockholm, Sweden",
+    },
+    {
+      y: 2000,
+      l: "Gainesville, FL, USA",
+    },
+    {
+      y: 2002,
+      l: "Barcelona, Spain",
+    },
+    {
+      y: 2004,
+      l: "Fortaleza, Brazil",
+    },
+    {
+      y: 2006,
+      l: "Seoul, Korea",
+    },
+    {
+      y: 2008,
+      l: "Berlin, Germany",
+    },
+    {
+      y: 2010,
+      l: "Melbourne, Australia",
+    },
+    {
+      y: 2012,
+      l: "Edmonton, Canada",
+    },
+    {
+      y: 2014,
+      l: "Coimbra, Portugal",
+    },
+    {
+      y: 2016,
+      l: "Jinan, China",
+    },
+    {
+      y: 2018,
+      l: "Norman, OK, USA",
+    },
+    {
+      y: 2020,
+      l: "No meeting",
+    },
+    {
+      y: 2022,
+      l: "Lublin, Poland",
+    },
+  ];
+  return (
+    <Flex direction={"column"} minH={"80vh"} mb={'2%'}>
+      <Heading textAlign={"center"} mt={"2%"} color="#211D70">
+        Symposium History
+      </Heading>
+      <TableContainer padding={"2% 2%"}>
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th>Year</Th>
+              <Th>Location</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {data.map((d) => {
+              return (
+                <Tr>
+                  <Td>{d.y}</Td>
+                  <Td>{d.l}</Td>
+                </Tr>
+              );
+            })}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </Flex>
+  );
 }
--- src/app/symposiumoverview/page.js
+++ src/app/symposiumoverview/page.js
@@ -1,9 +1,36 @@
-"use client"
-import { Box } from "@chakra-ui/react";
+"use client";
+import {
+  Box,
+  Flex,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
 import React from "react";
 import Puc from "../HomePage/Puc";
 export default function symposiumoverview() {
-return(
-    <Puc />
-)
+  return (
+    <Flex direction={"column"} minH={"80vh"}>
+      <Heading textAlign={"center"} mt={"2%"} color="#211D70">
+        Symposium Overview
+      </Heading>
+      <Text mt={"2%"} padding={"0 2%"}>
+        The “Surfactants in Solution” symposium series was initiated in 1976,
+        when it was first held in Albany, New York, under the chairmanship of
+        Dr. Kash Mittal. Two more meetings were held in the USA (Knoxville and
+        Potsdam) before the symposium became truly international: Lund (1982),
+        Bordeaux (1984), New Delhi (1986), Ottawa (1988), Gainesville (1990),
+        Varna (1992), Caracas (1994), Jerusalem (1996), Stockholm (1998),
+        Gainesville (2000), Barcelona (2002), Fortaleza (2004), Seoul (2006),
+        Berlin (2008), Melbourne (2010), Edmonton (2012), Coimbra (2014), Jinan
+        (2016), Norman (2018) and Lublin (2022). These symposia allow
+        academicians and scientists from industry to present their work in basic
+        aspects of surfactants and interfacial science and also meet people and
+        network with their peers working in these all important areas. The next
+        edition of the symposium is scheduled to be held at the K. K. Birla Goa
+        Campus of Birla Institute of Technology &amp; Science (BITS) Pilani from
+        June 16-21, 2024. We look forward to hosting you in Goa in June 2024 and
+        to make SIS2024 a memorable scientific and cultural experience.
+      </Text>
+    </Flex>
+  );
 }
